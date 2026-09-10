db09c7bd1ca0232a37e409a2dac9743595b25d45
Richard Melkonian <r.v.melkonian@gmail.com>
2026-09-10 10:25:23 +0100

    Refresh Roman Open Graph preview

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -239,16 +239,16 @@ export const Head = () => (
     <meta property="og:description" content={seoDescription} />
     <meta property="og:site_name" content="Porto" />
     <meta property="og:locale" content="en_GB" />
-    <meta property="og:image" content="https://www.portolabs.xyz/images/og-image.png?v=20260910-2" />
-    <meta property="og:image:secure_url" content="https://www.portolabs.xyz/images/og-image.png?v=20260910-2" />
+    <meta property="og:image" content="https://www.portolabs.xyz/images/og-image-roman-1200x630.png?v=20260910-3" />
+    <meta property="og:image:secure_url" content="https://www.portolabs.xyz/images/og-image-roman-1200x630.png?v=20260910-3" />
     <meta property="og:image:type" content="image/png" />
-    <meta property="og:image:width" content="1731" />
-    <meta property="og:image:height" content="909" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
     <meta property="og:image:alt" content="Porto: Spotify takes 30%. You get pennies." />
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:title" content="Porto | Spotify takes 30%. You get pennies." />
     <meta name="twitter:description" content={seoDescription} />
-    <meta name="twitter:image" content="https://www.portolabs.xyz/images/og-image.png?v=20260910-2" />
+    <meta name="twitter:image" content="https://www.portolabs.xyz/images/og-image-roman-1200x630.png?v=20260910-3" />
     <meta name="twitter:image:alt" content="Porto: Spotify takes 30%. You get pennies." />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
